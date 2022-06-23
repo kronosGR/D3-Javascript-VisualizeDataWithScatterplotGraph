0646bd97551a45f94870d03f936a5cd8e84f4d26
kronosGR <geo.elgeo@gmail.com>
2022-06-23 12:50:22 +0200

    updated axis and started the dots

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const width = 800;
-const height = 800;
+const height = 700;
 
 const svg = d3
   .select('#svg_container')
@@ -28,11 +28,29 @@ d3.json(
   const xAxis = d3.axisBottom(scaleX);
 
   const scaleY = d3
-    .scaleTime([d3.min(data, (d) => d.Time), d3.max(data, (d) => d.Time)])
-    .domain([])
+    .scaleTime()
+    .domain([d3.min(data, (d) => d.Time), d3.max(data, (d) => d.Time)])
     .range([0, height]);
   const yAxis = d3.axisLeft(scaleY);
 
-  svg.append('g').call(xAxis);
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${height - 1})`)
+    .call(xAxis);
   svg.append('g').call(yAxis);
+
+  data.forEach((d) => {
+    const parsed = d.Time.split(':');
+    d.Time = new Date(d.Year, 0, 1, parsed[0], parsed[1]);
+    console.log(d);
+  });
+
+  svg
+    .selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cy', (d) => d.Time)
+    .attr('cx', (d) => d.Date)
+    .attr('r', 6);
 });
