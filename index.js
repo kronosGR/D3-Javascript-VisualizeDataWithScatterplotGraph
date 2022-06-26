c7d6ea636fbeceda02d29afe44373e4f7bafe5b9
kronosGR <geo.elgeo@gmail.com>
2022-06-26 13:06:13 +0200

    small bug in time converstion

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ d3.json(
   */
 
   data.forEach((d) => {
-    //console.log(d);
+    // console.log(d.Time);
     const parsed = d.Time.split(':');
-    d.Time = new Date(2000, 0, 1, parsed[0], parsed[1]);
+    d.Time = new Date(2000, 0, 1, 0, parsed[0], parsed[1]);
   });
 
   const scaleX = d3
